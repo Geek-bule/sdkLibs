74a0840071d2847b620bc6c866dae0bae81d069c
GeekBlue <apple@appledeMacBook-Air.local>
2018-01-14 11:05:04 +0800

    geek
    基础文件夹补充上传

---
+++ herman/herman-demo-springmvc-aggregation/herman-demo-app/src/main/java/com/herman/demo/common/controller/BaseController.java
@@ -0,0 +1,12 @@
+package com.herman.demo.common.controller;
+
+/**
+ * Created by Administrator on 2018/1/2.
+ */
+public class BaseController {
+
+    public String getMemberId() {
+        return "";
+    }
+
+}
---
+++ herman/herman-demo-springmvc-aggregation/herman-demo-bean/src/main/java/com/herman/demo/service/game/impl/GameRecordServiceImpl.java
@@ -0,0 +1,29 @@
+package com.herman.demo.service.game.impl;
+
+import com.herman.demo.dao.game.IGameRecordDAO;
+import com.herman.demo.entity.game.GameRecord;
+import com.herman.demo.service.game.IGameRecordService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by herman on 2018/1/1.
+ */
+@Service
+public class GameRecordServiceImpl implements IGameRecordService {
+
+//    @Autowired
+//    private IGameRecordDAO gameRecordDao;
+//
+//    /**
+//     * 查询游戏记录
+//     * @param gameId
+//     * @param dgAccount
+//     * @return
+//     */
+//    @Override
+//    public GameRecord getGameRecord(Long gameId, String dgAccount) {
+//        return gameRecordDao.selectByGameIdAndDgAccount(gameId, dgAccount);
+//    }
+
+}
